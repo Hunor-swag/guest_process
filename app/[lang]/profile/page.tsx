e498ec990f6b90624890c04b878e3c77b96d7c9d
Hunor-swag <mogyorosi.hunor@gmail.com>
2023-04-22 00:38:59 +0200

    friday commit

--- app/[lang]/profile/page.tsx
+++ app/[lang]/profile/page.tsx
@@ -1,16 +1,1 @@
 // profil elkeszitese nev, email, telefonszam
-"use client";
-import { createContext } from "react";
-import React from "react";
-
-const ProfileContext = createContext(Profile);
-
-function Profile() {
-  return (
-    <ProfileContext.Provider>
-      <div>Profile</div>
-    </ProfileContext.Provider>
-  );
-}
-
-export default Profile;
