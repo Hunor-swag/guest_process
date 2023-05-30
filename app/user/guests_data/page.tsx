bc18fe5c179b8318af929371b1bf85e4c63ea9cd
Hunor-swag <mogyorosi.hunor@gmail.com>
2023-05-30 09:17:11 +0200

    Deploying...

--- app/user/guests_data/page.tsx
+++ app/user/guests_data/page.tsx
@@ -2,16 +2,18 @@ import GuestsData from "@/components/guests_data/GuestsData";
 import { Guest } from "@/types/typings";
 
 async function getData() {
-  const res = await fetch("http://localhost:3000/api/guests");
-  if (!res.ok) {
-    throw new Error("Failed to fetch data");
-  }
+  // const res = await fetch("http://localhost:3000/api/guests");
+  // const json = await res.json();
+  // if (json.errors) {
+  //   throw new Error("Failed to fetch API");
+  // }
 
-  return res.json();
+  // return json.data;
+  return new Array<Guest>();
 }
 
 export default async function GuestsPage() {
   const guests = await getData();
 
-  return <GuestsData guests={guests.data} />;
+  return <GuestsData guests={guests} />;
 }
