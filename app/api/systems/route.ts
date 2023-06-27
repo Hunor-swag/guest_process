2710a92b0752b10fd8ef9d26f7381c765b1eafd1
Hunor-swag <mogyorosi.hunor@gmail.com>
2023-06-27 11:05:49 +0200

    corrected small mistake

---
+++ app/api/systems/route.ts
@@ -0,0 +1,27 @@
+import { query } from "@/lib/db";
+import { NextRequest, NextResponse } from "next/server";
+import bcrypt from "bcryptjs";
+
+export async function GET(req: NextRequest) {
+  try {
+    const queryString = `
+      SELECT * FROM hotel_systems;
+    `;
+
+    const results = await query("control_panel", queryString, []);
+
+    return new NextResponse(JSON.stringify(results), {
+      status: 200,
+      headers: { "Content-Type": "application/json" },
+    });
+  } catch (error: any) {
+    let error_response = {
+      status: "error",
+      message: error.message,
+    };
+    return new NextResponse(JSON.stringify(error_response), {
+      status: 500,
+      headers: { "Content-Type": "application/json" },
+    });
+  }
+}
